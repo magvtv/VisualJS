b2873a42bc6a7c6dc6e4644aece8b6a008d26340
magvtv <magutujr@gmail.com>
2024-01-14 18:23:04 +0300

    stranded in sketch_5, the grid lines are not okay

--- Sketches/Sketch_5.js
+++ Sketches/Sketch_5.js
@@ -1,46 +1,52 @@
+// import canvasSketch from 'canvas-sketch';
 const canvasSketch = require('canvas-sketch');
-// import canvasSketch from 'canvas-sketch'
 
 const settings = {
-  dimensions: [1080, 1080]
-
+	dimensions: [ 1080, 1080 ]
 };
 
 const sketch = () => {
-  return ({ context, width, height }) => {
-    context.fillStyle = '#242423';
-    // context.fillStyle = '#F5CB5C'
-    context.fillRect(0, 0, width, height);
+	return ({ context, width, height }) => {
+		// context.fillStyle = '#242423';
+		context.fillStyle = '#F5CB5C';
+		context.fillRect(0, 0, width, height);
 
-    const columns = 4, rows = 3
-    const numUnits = columns * rows
-    const grid_w = width * .8, grid_h = height * .8
-    const unit_w = grid_w / columns, unit_h = grid_h / rows
-    const marginX = (grid_w - width) * -.5, marginY = (height - grid_h) * .5
+		const columns = 4,
+			rows = 4,
+			numUnits = columns * rows,
+			grid_w = width * 0.75,
+			grid_h = height * 0.75,
+			cell_w = grid_w / columns,
+			cell_h = grid_h / rows,
+			margin_x = (width - grid_w) * 0.5,
+			margin_y = (height - grid_h) * 0.5;
 
-    for (let i = 0; i < numUnits; i++) {
-      const cl = i % columns, rw = Math.floor(i / cl)
-      const ox = cl * unit_w, oy = rw * unit_h
-      const wd = unit_w * .8
-      // const rnd = random.noise2D(ox, oy), turn = rnd * Math.PI
+		for (let i = 0; i < numUnits; i++) {
+			const cl = i % columns,
+				rw = Math.floor(i / cl),
+				ox = cl * cell_w,
+				oy = rw * cell_h,
+				wd = cell_w * 0.8,
+				ht = cell_h * 0.8;
+			// const rnd = random.noise2D(ox, oy), turn = rnd * Math.PI
 
-      context.save();
-      context.translate(ox, oy);
-      context.translate(marginX, marginY);
-      context.translate(unit_w * .5, unit_h * .5);
-      // context.rotate(turn)
-      // context.translate(wd * .5, ht * .5)
-      
-      context.lineWidth = 30;
-      context.beginPath();
-      context.moveTo(wd * .5, 0);
-      context.lineTo(wd * -.5, 0);
-      context.stroke();
+			context.save();
+			context.translate(ox, oy);
+			context.translate(margin_x, margin_y);
+			context.translate(cell_w * 0.5, cell_h * 0.5);
 
+			context.lineWidth = 10;
 
-      context.restore();
-    };
-  };
+			context.beginPath();
+			context.moveTo(wd * -0.5, 0);
+			context.lineTo(ht * 0.5, 0);
+			context.stroke();
+
+			context.restore();
+			// context.rotate(turn)
+			// context.translate(wd * .5, ht * .5)
+		}
+	};
 };
 
 canvasSketch(sketch, settings);

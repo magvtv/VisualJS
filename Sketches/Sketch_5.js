765568dd5b20cb281499b3a05959fd6d4e3b9368
magvtv <magutujr@gmail.com>
2024-01-15 08:18:17 +0300

    finding my foot in the creative code game

--- Sketches/Sketch_5.js
+++ Sketches/Sketch_5.js
@@ -2,7 +2,7 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-	dimensions: [ 1080, 1080 ]
+	dimensions: [1080, 1080],
 };
 
 const sketch = () => {
@@ -11,17 +11,18 @@ const sketch = () => {
 		context.fillStyle = '#F5CB5C';
 		context.fillRect(0, 0, width, height);
 
-		const columns = 4,
-			rows = 4,
+		const columns = 5,
+			rows = 5,
 			numUnits = columns * rows,
-			grid_w = width * 0.75,
-			grid_h = height * 0.75,
+			grid_w = width * 0.8,
+			grid_h = height * 0.8,
 			cell_w = grid_w / columns,
 			cell_h = grid_h / rows,
 			margin_x = (width - grid_w) * 0.5,
 			margin_y = (height - grid_h) * 0.5;
 
-		for (let i = 0; i < numUnits; i++) {
+		for (let i = 0; i <
+       numUnits; i++) {
 			const cl = i % columns,
 				rw = Math.floor(i / cl),
 				ox = cl * cell_w,
@@ -35,7 +36,7 @@ const sketch = () => {
 			context.translate(margin_x, margin_y);
 			context.translate(cell_w * 0.5, cell_h * 0.5);
 
-			context.lineWidth = 10;
+			context.lineWidth = 5;
 
 			context.beginPath();
 			context.moveTo(wd * -0.5, 0);
